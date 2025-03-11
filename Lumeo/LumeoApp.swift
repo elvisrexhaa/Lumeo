6668c99d6f576e9aaa9f45f3432cee196ee7bda8
elvisrexhaa <elvis2000@hotmail.co.uk>
2025-03-11 13:15:23 +0000

    Initial Commit

---
+++ Lumeo/LumeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LumeoApp.swift
+//  Lumeo
+//
+//  Created by Elvis Rexha on 11/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct LumeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LumeoTests/LumeoTests.swift
@@ -0,0 +1,17 @@
+//
+//  LumeoTests.swift
+//  LumeoTests
+//
+//  Created by Elvis Rexha on 11/03/2025.
+//
+
+import Testing
+@testable import Lumeo
+
+struct LumeoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
